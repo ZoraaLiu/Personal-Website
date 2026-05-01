66ca81e58a292f491eea93d289a2fc7e707cd7e4
Zora Liu <zoraliu658@gmail.com>
2026-05-01 17:33:10 -0400

    Update some settings

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Website/', // Must match your repo name exactly
+  base: '/zl-profolio/', // Must match your GitHub Pages repo path
 });
  
